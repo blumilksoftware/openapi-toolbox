943f53cc683f87f568e3511aa810418e233104d4
krzysztofrewak <krzysztof.rewak@gmail.com>
2023-05-18 20:43:14 +0200

    #1 - package skeleton

---
+++ src/OpenApiToolboxServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\OpenApiToolbox;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenApiToolboxServiceProvider extends ServiceProvider
+{
+}
