f321eaff9bf703e3cffa53525a8816c75a5c337f
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2023-08-24 12:03:03 +0200

    #11 - builders

--- src/OpenApiSpecification/DocumentationBuilders/CachedSingleFileDocumentationBuilder.php
+++ src/OpenApiSpecification/DocumentationBuilders/CachedSingleFileDocumentationBuilder.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Blumilk\OpenApiToolbox\OpenApiSpecification\DocumentationBuilders;
 
-class CachedMultipleFilesDocumentationBuilder extends MultipleFilesDocumentationBuilder
+class CachedSingleFileDocumentationBuilder extends SingleFileDocumentationBuilder
 {
     use CacheActions;
 
