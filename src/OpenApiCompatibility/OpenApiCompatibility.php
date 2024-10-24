722b0d539277d784dc7d7af59f8da0f15c707af6
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2024-10-24 10:38:20 +0200

    #34 - dependencies update fix (#35)
    * #34 - dependencies update fix
    
    * #34 - PHP 8.3 required

--- src/OpenApiCompatibility/OpenApiCompatibility.php
+++ src/OpenApiCompatibility/OpenApiCompatibility.php
@@ -6,12 +6,37 @@ namespace Blumilk\OpenApiToolbox\OpenApiCompatibility;
 
 use Blumilk\OpenApiToolbox\OpenApiSpecification\SpecificationBuilder;
 use Illuminate\Contracts\Config\Repository;
+use Kirschbaum\OpenApiValidator\Exceptions\UnknownParserForFileTypeException;
+use Kirschbaum\OpenApiValidator\Exceptions\UnknownSpecFileTypeException;
 use Kirschbaum\OpenApiValidator\ValidatesOpenApiSpec;
 use KrzysztofRewak\OpenApiMerge\Writer\Exception\InvalidFileTypeException;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
 
 trait OpenApiCompatibility
 {
     use ValidatesOpenApiSpec;
+
+    /**
+     * @throws InvalidFileTypeException
+     * @throws UnknownParserForFileTypeException
+     * @throws UnknownSpecFileTypeException
+     */
+    public function getOpenApiValidatorBuilder(): ValidatorBuilder
+    {
+        if (!isset($this->openApiValidatorBuilder)) {
+            $specType = $this->getSpecFileType();
+
+            if ($specType === "json") {
+                $this->openApiValidatorBuilder = (new ValidatorBuilder())->fromJson($this->getOpenApiSpec());
+            } elseif ($specType === "yaml") {
+                $this->openApiValidatorBuilder = (new ValidatorBuilder())->fromYaml($this->getOpenApiSpec());
+            } else {
+                throw new UnknownParserForFileTypeException("Unknown parser for file type {$specType}");
+            }
+        }
+
+        return $this->openApiValidatorBuilder;
+    }
 
     /**
      * @throws InvalidFileTypeException
